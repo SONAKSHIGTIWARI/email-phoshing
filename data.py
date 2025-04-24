07f311e8aac925dcdd97d1ea473dc9a2630a4ea4
SONAKSHIGTIWARI <142566567+SONAKSHIGTIWARI@users.noreply.github.com>
2025-04-24 18:10:58 +0530

    count complete subarray

--- data.py
+++ data.py
@@ -1,21 +1,14 @@
 class Solution:
-    def countLargestGroup(self, n: int) -> int:
-        freq=[0]*37
-        maxF, sz=1, 1
-        freq[1]=1
-        for x in range(2, n+1):
-            digit_sum, y=0, x
-            while y>0:
-                q, r=divmod(y, 10)
-                digit_sum+=r
-                y=q
-            freq[digit_sum]+=1
-            f=freq[digit_sum]
-            if f==maxF: 
-                sz+=1
-            elif f>maxF:
-                maxF=f
-                sz=1
-        return sz
-
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        totalDistinct = len(set(nums))
+        n = len(nums)
+        count = 0
         
+        for i in range(n):
+            seen = set()
+            for j in range(i, n):
+                seen.add(nums[j])
+                if len(seen) == totalDistinct:
+                    count += n - j
+                    break
+        return count
