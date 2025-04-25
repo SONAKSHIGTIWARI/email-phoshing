f4663f690f03ce4c4b016f1283f2563c53cb1a8d
SONAKSHIGTIWARI <142566567+SONAKSHIGTIWARI@users.noreply.github.com>
2025-04-25 20:59:07 +0530

    Top K Frequent Elements

--- data.py
+++ data.py
@@ -1,14 +1,15 @@
 class Solution:
-    def countCompleteSubarrays(self, nums: List[int]) -> int:
-        totalDistinct = len(set(nums))
-        n = len(nums)
-        count = 0
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        counter = {}
+        for n in nums:
+            counter[n] = 1 + counter.get(n, 0)
         
-        for i in range(n):
-            seen = set()
-            for j in range(i, n):
-                seen.add(nums[j])
-                if len(seen) == totalDistinct:
-                    count += n - j
-                    break
-        return count
+        for key, val in counter.items():
+            heapq.heappush(heap, (-val, key))
+        
+        res = []
+        while len(res) < k:
+            res.append(heapq.heappop(heap)[1])
+        
+        return res
