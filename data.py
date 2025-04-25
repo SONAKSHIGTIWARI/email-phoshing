4c62562f433c1cfe581c9d596576acd8794f94c6
SONAKSHIGTIWARI <142566567+SONAKSHIGTIWARI@users.noreply.github.com>
2025-04-25 21:02:09 +0530

    Super ugly Number..

--- data.py
+++ data.py
@@ -1,15 +1,17 @@
+from collections import defaultdict
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        heap = []
-        counter = {}
-        for n in nums:
-            counter[n] = 1 + counter.get(n, 0)
-        
-        for key, val in counter.items():
-            heapq.heappush(heap, (-val, key))
-        
-        res = []
-        while len(res) < k:
-            res.append(heapq.heappop(heap)[1])
-        
-        return res
+    def countInterestingSubarrays(self, nums: List[int], m: int, k: int) -> int:
+        count = 0
+        mod_map = defaultdict(int)
+        mod_map[0] = 1
+        total = 0
+
+        for num in nums:
+            if num % m == k:
+                count += 1
+            remainder = (count - k) % m
+            total += mod_map[remainder]
+            mod_map[count % m] += 1
+
+        return total        
