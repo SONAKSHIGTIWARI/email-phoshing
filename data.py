185799dbd11a21b415953b223be560a5246daca5
SONAKSHIGTIWARI <142566567+SONAKSHIGTIWARI@users.noreply.github.com>
2025-04-27 20:43:53 +0530

    maximum product of word

--- data.py
+++ data.py
@@ -1,24 +1,11 @@
 class Solution:
-    def removeDuplicateLetters(self, s: str) -> str:
-        # Dictionary to store the last occurrence of each character
-        last_occur = {}
-
-        # Record the last occurrence of each character
-        for i, char in enumerate(s):
-            last_occur[char] = i        
-
-        stack = []  # Stack to store characters in the desired order
-        visited = set()  # Set to keep track of visited characters
-
-        for i in range(len(s)):
-            if s[i] in visited:
-                continue  # Skip if the character is already visited
-
-            # If the top of the stack is greater than s[i] and will occur later again, remove from stack
-            while stack and s[i] < stack[-1] and i < last_occur.get(stack[-1], -1):
-                visited.remove(stack.pop())
-
-            visited.add(s[i])  # Mark as visited
-            stack.append(s[i])  # Add to the stack
+    def maxProduct(self, words: List[str]) -> int:
+        n=len(words)                        
+        char_set = [set(words[i]) for i in range(n)] # precompute hashset for each word                                                  
+        max_val = 0
+        for i in range(n):
+            for j in range(i+1, n):
+                if not (char_set[i] & char_set[j]): # if nothing common
+                    max_val=max(max_val, len(words[i]) * len(words[j]))
         
-        return ''.join(stack)  # Concatenate the characters remaining in the stack
+        return max_val   
